93c7b4b0ff9a95f8e59bd7fb41bf2acc787a0b13
Christopher <olanochristopher@gmail.com>
2023-02-15 22:21:01 +0800

    creating generic/reusable search-box component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -40,14 +41,7 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input className='search-box' type='search' placeholder='Search monsters' onChange={onSearchChange} />
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })} */}
+        <SearchBox className='search-box' onChangeHandler={onSearchChange} placeholder='search monster' />
         <CardList monsters={filteredMonsters} />
       </div>
     );
