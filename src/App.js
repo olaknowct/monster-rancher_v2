2ab53a6ddd1b7ec85ba22308ae763c295dac862f
Christopher <olanochristopher@gmail.com>
2023-02-15 22:08:21 +0800

    create reusable card list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -40,13 +41,14 @@ class App extends Component {
     return (
       <div className='App'>
         <input className='search-box' type='search' placeholder='Search monsters' onChange={onSearchChange} />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
