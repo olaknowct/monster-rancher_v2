ff8740b45dd2b71cc10cc66c14def93e4bb222e5
Christopher <olanochristopher@gmail.com>
2023-02-17 11:27:59 +0800

    transforming class app into functional a functional component use hook useState for states use hook useEffect for side effects

--- src/App.js
+++ src/App.js
@@ -1,52 +1,88 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  console.log('render');
+  const [searchField, setSearchField] = useState(''); // returns [value, setValue function]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-  }
-
-  componentDidMount() {
+  // Run once onmount
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toUpperCase();
-    this.setState(
-      () => {
-        return { searchField };
-      },
-      () => console.log(this.state)
-    );
+  // Run Once, and rerun again when monsters or search field is different from previous state
+  useEffect(() => {
+    console.log('effect firing');
+
+    const newFilteredMonsters = monsters.filter((monster) => monster.name.toLocaleLowerCase().startsWith(searchField));
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+  console.log('after effect render');
+
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters.filter((monster) => monster.name.toUpperCase().startsWith(searchField));
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monster Rolodex</h1>
+      <SearchBox className='monster-search-box' onChangeHandler={onSearchChange} placeholder='search monster' />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+// class App extends Component {
+//   constructor() {
+//     super();
 
-    return (
-      <div className='App'>
-        <h1 className='app-title'>Monster Rancher</h1>
-        <SearchBox className='monster-search-box' onChangeHandler={onSearchChange} placeholder='search monster' />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+//   }
+
+//   componentDidMount() {
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then((response) => response.json())
+//   .then((users) =>
+//     this.setState(() => {
+//       return { monsters: users };
+//     })
+//   );
+//   }
+
+//   onSearchChange = (e) => {
+//     const searchField = e.target.value.toUpperCase();
+//     this.setState(
+//       () => {
+//         return { searchField };
+//       },
+//       () => console.log(this.state)
+//     );
+//   };
+
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonsters = monsters.filter((monster) => monster.name.toUpperCase().startsWith(searchField));
+
+//     return (
+//       <div className='App'>
+//         <h1 className='app-title'>Monster Rancher</h1>
+//         <SearchBox className='monster-search-box' onChangeHandler={onSearchChange} placeholder='search monster' />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
